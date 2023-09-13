0abe97d894fabc8c6c2a734dcde5427c9612e1b6
Jeremias Rodriguez <rodriguezjeremias09@gmail.com>
2023-09-12 23:10:36 -0300

    subiendo el mvn

---
+++ jer/src/main/java/com/argprog/jer/JeremiasApplication.java
@@ -0,0 +1,13 @@
+package com.argprog.jer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeremiasApplication.class, args);
+	}
+
+}
---
+++ jer/src/test/java/com/argprog/jer/JeremiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argprog.jer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeremiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
